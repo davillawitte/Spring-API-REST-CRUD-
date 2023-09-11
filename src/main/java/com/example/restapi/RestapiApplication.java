932dd2141b09eeea10ee38f1c1f7edb758bed89e
davilla.witte <rosangela.witte.103@ufrn.edu.br>
2023-09-10 22:08:10 -0300

    create Entity

---
+++ src/main/java/com/example/restapi/RestapiApplication.java
@@ -0,0 +1,14 @@
+package com.example.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+	
+//Método principal que Starta o projeto
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/restapi/entities/Contato.java
@@ -0,0 +1,55 @@
+package com.example.restapi.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+// definir os atributos de Contato como private, significa que eles só podem ser acessados e modificados por métodos dentro da própria classe (getters e setters).
+
+//informa que a classe é uma entidade que deve ser persistida no banco de dados
+@Entity
+public class Contato {
+    @Id //identificador da entidade
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // valor gerado automaticamente (autoincrement)
+    private Long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String telefone;
+    @Column(nullable = false)
+    private String email;
+    private String urlAvatar;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUrlAvatar() {
+        return urlAvatar;
+    }
+    public void setUrlAvatar(String urlAvatar) {
+        this.urlAvatar = urlAvatar;
+    }
+}
